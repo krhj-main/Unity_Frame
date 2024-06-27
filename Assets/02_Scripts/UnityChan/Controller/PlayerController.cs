796e35cd8f58bd068a69f786a911d5435c3b50a2
krhj-main <dygks6666@gmail.com>
2024-06-27 15:55:17 +0900

    cancle
    프로젝트에 버그가 생겨 새 프로젝트로 변경함 Unity_WorkFrame 이용

--- Assets/02_Scripts/UnityChan/Controller/PlayerController.cs
+++ Assets/02_Scripts/UnityChan/Controller/PlayerController.cs
@@ -5,42 +5,51 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float _speed = 3f;
+
+    //InputManager _input = new InputManager();
     // Start is called before the first frame update
     void Start()
     {
-        
+        //_input.KeyAction = OnKeyBoard;
+        UnityManagers.Input.KeyAction -= OnKeyBoard;
+        UnityManagers.Input.KeyAction += OnKeyBoard;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Q))
+        
+    }
+
+    void OnKeyBoard()
+    {
+        if (Input.GetKey(KeyCode.W))
         {
             //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.2f);
             //transform.rotation = Quaternion.LookRotation(Vector3.forward);
-            transform.Translate(Vector3.forward * Time.unscaledDeltaTime * 3f);
-           // transform.position += Vector3.forward * Time.deltaTime * _speed;
+            transform.Translate(Vector3.forward * Time.unscaledDeltaTime * _speed);
+            // transform.position += Vector3.forward * Time.deltaTime * _speed;
         }
         else if (Input.GetKey(KeyCode.S))
         {
             //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.2f);
             // transform.rotation = Quaternion.LookRotation(Vector3.back);
             transform.Translate(Vector3.back * Time.deltaTime * _speed);
-           // transform.position += Vector3.back * Time.deltaTime * _speed;
+            // transform.position += Vector3.back * Time.deltaTime * _speed;
         }
         else if (Input.GetKey(KeyCode.D))
         {
-           // transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
+            // transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
             // transform.rotation = Quaternion.LookRotation(Vector3.right);
             transform.Translate(Vector3.right * Time.deltaTime * _speed);
-           // transform.position += Vector3.right * Time.deltaTime * _speed;
+            // transform.position += Vector3.right * Time.deltaTime * _speed;
         }
         else if (Input.GetKey(KeyCode.A))
         {
             //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.2f);
             //transform.rotation = Quaternion.LookRotation(Vector3.left);
-             transform.Translate(Vector3.left * Time.deltaTime * _speed);
-           // transform.position += Vector3.left * Time.deltaTime * _speed;
+            transform.Translate(Vector3.left * Time.deltaTime * _speed);
+            // transform.position += Vector3.left * Time.deltaTime * _speed;
         }
     }
 }
--- Assets/02_Scripts/UnityChan/Managers/UnityManagers.cs
+++ Assets/02_Scripts/UnityChan/Managers/UnityManagers.cs
@@ -4,6 +4,26 @@ using UnityEngine;
 
 public class UnityManagers : MonoBehaviour
 {
+    static UnityManagers U_instance;
+    static UnityManagers U_Instance
+    {
+        get { return U_instance; }
+    }
+
+    InputManager _input = new InputManager();
+
+    public static InputManager Input
+    {
+        get { return U_Instance._input; }
+    }
+
+    private void Awake()
+    {
+        if (U_instance == null)
+        {
+            U_instance = this;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +33,6 @@ public class UnityManagers : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        _input.OnUpdate();
     }
 }
